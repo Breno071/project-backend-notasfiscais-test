5925ff1ed666f91b91d6258d30f8816782c57559
Breno Narde Silva <brenonarde@gmail.com>
2023-09-26 18:46:21 -0300

    Adiciona o projeto de testes

---
+++ NotasFiscais.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace NotasFiscais.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            //Act
+            //Assert
+            Assert.True(true);
+        }
+    }
+}
